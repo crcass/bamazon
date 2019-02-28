08888d605217c329f63094ebc8dad947e081ef2b
crcass <ccass@live.com>
2019-02-28 16:15:14 -0600

    initial project setup

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('oled tv', 'electronics', 2000.00, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('smart phone', 'electronics', 650.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('gourmet coffee', 'food', 15.00, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('dog treats', 'pets', 15.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('dog collar', 'pets', 10.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('designer dog collar', 'pets', 50.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('ice cream', 'food', 4.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('romance novel', 'books', 19.95, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('sci-fi novel', 'books', 17.95, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('cheap coffee', 'food', 5.00, 400);
